21cd8d8fd57d4f5a7f637c9c388c02dd85521336
Maxine <maxinesun0128@yahoo.com>
2020-04-20 05:42:44 +0800

    update

--- lstm.py
+++ lstm.py
@@ -7,6 +7,7 @@ import pandas as pd
 from keras.models import Sequential, load_model
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import r2_score
 
 # 读取数据
 dataframe = pd.read_csv('SARS.csv',usecols=[1])
@@ -33,7 +34,7 @@ def create_dataset(dataset, timestep ):
         dataY.append(dataset[i + timestep ])
     return np.array(dataX),np.array(dataY)
 #训练数据太少 timestep 取2
-timestep  = 1
+timestep  = 3
 trainX,trainY  = create_dataset(dataset,timestep )
 
 trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
@@ -42,47 +43,71 @@ model = Sequential()
 model.add(LSTM(4, input_shape=(None,1)))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+model.fit(trainX, trainY, epochs=40, batch_size=1, verbose=2)
 model.save("LSTM.h5")
-
-trainPredict = model.predict(trainX)
-#反归一化
-trainPredict = scaler.inverse_transform(trainPredict)
-trainY = scaler.inverse_transform(trainY)
-
-
-plt.figure(figsize=(10, 8))
-plt.plot(trainY[1:])
-plt.plot(trainPredict)
-plt.title('训练集上的结果')
-plt.xlabel('天数')
-plt.ylabel('感染人群数目')
-plt.legend(['train', 'trainPredict'], loc='best')
-plt.show()
-
 
 
 # 读取新型冠状病毒数据进行测试
-dataframe = pd.read_csv('Shenzhen.csv',usecols=[1])
-dataset = dataframe.values
-dataset = dataset.astype('float32')
+dataframeI = pd.read_csv('Italy.csv',usecols=[1])
+dataframeS = pd.read_csv('Spain.csv',usecols=[1])
+dataframeU = pd.read_csv('USA.csv',usecols=[1])
+datasetI = dataframeI.values
+datasetS = dataframeS.values
+datasetU = dataframeU.values
+datasetI = datasetI.astype('float32')
+datasetS = datasetS.astype('float32')
+datasetU = datasetU.astype('float32')
 #归一化
 scaler = MinMaxScaler(feature_range=(0, 1))
-dataset = scaler.fit_transform(dataset)
+datasetI = scaler.fit_transform(datasetI)
+datasetS = scaler.fit_transform(datasetS)
+datasetU = scaler.fit_transform(datasetU)
 
-testdata,TEXTY  = create_dataset(dataset,1)
-res= model.predict(testdata)
+testdataI,TEXTYI  = create_dataset(datasetI,3)
+resI= model.predict(testdataI)
 # 结果反归一化
-res = scaler.inverse_transform(res)
-TEXTY = scaler.inverse_transform(TEXTY)
+resI = scaler.inverse_transform(resI)
+TEXTYI = scaler.inverse_transform(TEXTYI)
+scoreI = r2_score(TEXTYI, resI, multioutput='raw_values')
+print(scoreI)
 
+testdataS,TEXTYS  = create_dataset(datasetS,3)
+resS= model.predict(testdataS)
+# 结果反归一化
+resS = scaler.inverse_transform(resS)
+TEXTYS = scaler.inverse_transform(TEXTYS)
+scoreS = r2_score(TEXTYS, resS, multioutput='raw_values')
+print(scoreS)
+
+testdataU,TEXTYU  = create_dataset(datasetU,3)
+resU= model.predict(testdataU)
+# 结果反归一化
+resU = scaler.inverse_transform(resU)
+TEXTYU = scaler.inverse_transform(TEXTYU)
+scoreU = r2_score(TEXTYU, resU, multioutput='raw_values')
+print(scoreU)
 
 # 画图
 plt.figure(figsize=(10, 8))
-plt.plot(TEXTY[1:])
-plt.plot(res)
-plt.xlabel('天数')
-plt.ylabel('感染人群数目')
-plt.title('分析疫情的结果')
-plt.legend(['train', 'trainPredict'], loc='best')
+plt.plot(TEXTYI)
+plt.plot(resI)
+plt.xlabel('date(days)')
+plt.ylabel('number of comfirmed(ones)')
+plt.legend(['Actual', 'Predicted'], loc='best')
+plt.show()
+
+plt.figure(figsize=(10, 8))
+plt.plot(TEXTYS)
+plt.plot(resS)
+plt.xlabel('date(days)')
+plt.ylabel('number of comfirmed(ones)')
+plt.legend(['Actual', 'Predicted'], loc='best')
+plt.show()
+
+plt.figure(figsize=(10, 8))
+plt.plot(TEXTYU)
+plt.plot(resU)
+plt.xlabel('date(days)')
+plt.ylabel('number of comfirmed(ones)')
+plt.legend(['Actual', 'Predicted'], loc='best')
 plt.show()
---
+++ lstm1.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+import pandas as pd
+from keras.models import Sequential, load_model
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+# 读取数据
+dataframe = pd.read_csv('SARS.csv',usecols=[1])
+
+for i in range(dataframe['total'].shape[0]):
+    if dataframe['total'][i] == 0:
+        j=i+1
+        while(dataframe['total'][j]==0):
+            j+=1
+        dataframe['total'][i]=(dataframe['total'][i-1]+dataframe['total'][j])//2
+
+dataset = dataframe.values
+
+dataset = dataset.astype('float32')
+#归一化
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+def create_dataset(dataset, timestep ):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-timestep -1):
+        a = dataset[i:(i+timestep )]
+        dataX.append(a)
+        dataY.append(dataset[i + timestep ])
+    return np.array(dataX),np.array(dataY)
+#训练数据太少 timestep 取2
+timestep  = 7
+trainX,trainY  = create_dataset(dataset,timestep )
+
+trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+# create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(4, input_shape=(None,1)))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+model.save("LSTM.h5")
+
+
+# 读取新型冠状病毒数据进行测试
+dataframe = pd.read_csv('Italy.csv',usecols=[1])
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+#归一化
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+testdata,TEXTY  = create_dataset(dataset,7)
+res= model.predict(testdata)
+# 结果反归一化
+res = scaler.inverse_transform(res)
+TEXTY = scaler.inverse_transform(TEXTY)
+
+
+# 画图
+plt.figure(figsize=(10, 8))
+plt.plot(TEXTY)
+plt.plot(res)
+plt.xlabel('date(days)')
+plt.ylabel('number of comfirmed(ones)')
+plt.legend(['Actual', 'Predicted'], loc='best')
+plt.show()
